5048b1472ed9f7c2c69a680596702db1a105afd9
cslee0034 <cslee0034@gmail.com>
2024-03-30 19:49:39 +0900

    feat: footer 생성

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+export function Footer() {
+  return (
+    <footer className="border-t border-gray-200">
+      <div className="container">
+        <div className="flex flex-col items-center border-t border-gray-200 pb-12 pt-8 md:flex-row-reverse md:justify-between md:pt-6">
+          <p className="mt-6 text-sm text-gray-500 md:mt-0">
+            &copy; Copyright {new Date().getFullYear()}. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
