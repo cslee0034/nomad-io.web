8820983a7ee4f26fb3df4de40264950a3a1ebb79
cslee0034 <cslee0034@gmail.com>
2024-03-31 00:21:16 +0900

    feat: footer import default로 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import Header from "../components/header";
-import { Footer } from "../components/footer";
+import Footer from "../components/footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
