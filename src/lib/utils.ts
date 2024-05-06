5d02162bcb8fb506b88cf6f90e58206abbf0b8f1
cslee0034 <cslee0034@gmail.com>
2024-05-07 02:23:48 +0900

    feat: util에 cookie parsing 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function cookieParser(cookieHeader: string) {
+  return Object.fromEntries(
+    cookieHeader.split("; ").map((cookie) => cookie.split("="))
+  );
+}
