ec647b52f6feb3c6cdee588d619d09f6a1ee080c
cslee0034 <cslee0034@gmail.com>
2024-05-13 19:09:12 +0900

    feat: checkSuccess 함수 추가
    { success: true }를 반환하지 않으면 에러 발생

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { AxiosError, AxiosResponse } from "axios";
+import { SuccessResponse } from "../interfaces/success-response";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,4 +11,18 @@ export function cookieParser(cookieHeader: string) {
   return Object.fromEntries(
     cookieHeader.split("; ").map((cookie) => cookie.split("="))
   );
+}
+
+export function checkSuccess(response: SuccessResponse) {
+  if (!response || !response?.success) {
+    const error = response as unknown as AxiosError;
+    throw new Error(error?.message || "An error occurred during login");
+  }
+}
+
+export function checkSuccessNextApi(response: AxiosResponse) {
+  if (!response || !response?.data?.success) {
+    const error = response as unknown as AxiosError;
+    throw new Error(error?.message || "An error occurred during login");
+  }
 }
