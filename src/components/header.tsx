7db6371aeb9fbe74517e41bb235f4e89a53f8d24
cslee0034 <cslee0034@gmail.com>
2024-05-13 20:26:10 +0900

    feat: login 여부 zustand store에서 가져오도록 변경

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Sidebar from "./sidebar";
+import { useUserStore } from "../stores/useUserStore";
 
 export default function Header() {
-  const isLoggedIn = true;
+  const { isLoggedIn } = useUserStore();
 
   return (
     <header className="top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
